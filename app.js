6f404553507c38b36e98eb954e4be88941b9924a
santiagolealh <santiago.leal@atlanticsoft.us>
2022-08-10 13:26:23 -0500

    se separaron los routes de notes y users

--- app.js
+++ app.js
@@ -2,18 +2,23 @@ require('dotenv').config()
 require('./database')
 
 const express = require('express');
-const body_parser = require('body-parser');
-const routes = require('./src/routes/routes');
+const notes_routes = require('./src/routes/notes-routes');
+const user_routes = require('./src/routes/users-routes');
 const validate_token = require('./src/middleware/middleware');
 
 const app = express();
 
+// Initial route
+app.get("/", (req, res) => { 
+    res.send({message: "connected"});
+});
+
 // Middleware
-app.use(body_parser.urlencoded({ extended: false }));
-app.use(body_parser.json())
+app.use(express.json())
 app.use(validate_token)
 
-routes(app);
+notes_routes(app);
+user_routes(app);
 
 app.listen(process.env.PORT, () => {
     console.log(`Example app listening at http://localhost:${process.env.PORT}`)
